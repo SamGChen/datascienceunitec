af2beb6826597ebe37dc105fb82db691fd2ee8b7
SamGChen <101948824+SamGChen@users.noreply.github.com>
2022-03-20 12:34:40 +1300

    Add files via upload

---
+++ Lab 1-2.R
@@ -0,0 +1,24 @@
+## Q12
+hw1_data[1:2,]
+
+## Q13
+hw1_data
+
+## Q14
+hw1_data[47,Ozone]
+
+## Q15
+summary(hw1_data)
+
+## Q16
+summary(hw1_data)
+
+## Q17
+y<-hw1_data[hw1_data$Month=="5",]
+max(y[,1],na.rm = TRUE)
+
+
+## Q28
+y <- iris[iris$Species=="virginica",]
+mean(y[,1],na.rm = TRUE)
+
